3b551506a2a36bac8878ca03030101a1984b6fbd
Dimitra Chatzichrysou <dimitra.chatzichrysou@cern.ch>
2021-06-15 13:12:20 +0000

    Add sidecar for php-fpm metrics exporter

--- controllers/drupalsite_resources.go
+++ controllers/drupalsite_resources.go
@@ -575,7 +575,7 @@ func deploymentForDrupalSite(currentobject *appsv1.Deployment, databaseSecret st
 	if err != nil {
 		return newApplicationError(err, ErrFunctionDomain)
 	}
-	drupallogsResources, err := resourceRequestLimit("10Mi", "1m", "15Mi", "5m")
+	phpfpmexporterResources, err := resourceRequestLimit("10Mi", "1m", "15Mi", "5m")
 	if err != nil {
 		return newApplicationError(err, ErrFunctionDomain)
 	}
@@ -598,7 +598,7 @@ func deploymentForDrupalSite(currentobject *appsv1.Deployment, databaseSecret st
 		currentobject.Annotations = map[string]string{}
 		currentobject.Annotations["alpha.image.policy.openshift.io/resolve-names"] = "*"
 		currentobject.Spec.Template.ObjectMeta.Annotations = map[string]string{}
-		currentobject.Spec.Template.Spec.Containers = []corev1.Container{{Name: "nginx"}, {Name: "php-fpm"}, {Name: "drupal-logs"}}
+		currentobject.Spec.Template.Spec.Containers = []corev1.Container{{Name: "nginx"}, {Name: "php-fpm"}, {Name: "php-fpm-exporter"}}
 
 		// This annotation is required to trigger new rollout, when the imagestream gets updated with a new image for the given tag. Without this, deployments might start running with
 		// a wrong image built from a different build, that is left out on the node
@@ -677,7 +677,6 @@ func deploymentForDrupalSite(currentobject *appsv1.Deployment, databaseSecret st
 		for i, container := range currentobject.Spec.Template.Spec.Containers {
 			switch container.Name {
 			case "nginx":
-				currentobject.Spec.Template.Spec.Containers[i].Name = "nginx"
 				// Set to always due to https://gitlab.cern.ch/drupal/paas/drupalsite-operator/-/issues/54
 				currentobject.Spec.Template.Spec.Containers[i].ImagePullPolicy = "Always"
 				currentobject.Spec.Template.Spec.Containers[i].Ports = []corev1.ContainerPort{{
@@ -714,7 +713,6 @@ func deploymentForDrupalSite(currentobject *appsv1.Deployment, databaseSecret st
 				currentobject.Spec.Template.Spec.Containers[i].Resources = nginxResources
 
 			case "php-fpm":
-				currentobject.Spec.Template.Spec.Containers[i].Name = "php-fpm"
 				currentobject.Spec.Template.Spec.Containers[i].Command = []string{"/run-php-fpm.sh"}
 				// Set to always due to https://gitlab.cern.ch/drupal/paas/drupalsite-operator/-/issues/54
 				currentobject.Spec.Template.Spec.Containers[i].ImagePullPolicy = "Always"
@@ -769,23 +767,28 @@ func deploymentForDrupalSite(currentobject *appsv1.Deployment, databaseSecret st
 				}
 				currentobject.Spec.Template.Spec.Containers[i].Resources = phpfpmResources
 
-			case "drupal-logs":
-				currentobject.Spec.Template.Spec.Containers[i].Name = "drupal-logs"
-				currentobject.Spec.Template.Spec.Containers[i].Command = []string{"sh", "-c", "touch /drupal-data/drupal.log;" + " tail -f /drupal-data/drupal.log"}
+			case "php-fpm-exporter":
 				// Set to always due to https://gitlab.cern.ch/drupal/paas/drupalsite-operator/-/issues/54
 				currentobject.Spec.Template.Spec.Containers[i].ImagePullPolicy = "Always"
+				// Port on which to expose metrics
 				currentobject.Spec.Template.Spec.Containers[i].Ports = []corev1.ContainerPort{{
-					ContainerPort: 8081,
-					Name:          "drupal-logs",
+					ContainerPort: 9253,
+					Name:          "php-fpm-metrics",
 					Protocol:      "TCP",
 				}}
-				currentobject.Spec.Template.Spec.Containers[i].VolumeMounts = []corev1.VolumeMount{
+				currentobject.Spec.Template.Spec.Containers[i].Env = []corev1.EnvVar{
 					{
-						Name:      "drupal-directory-" + d.Name,
-						MountPath: "/drupal-data",
+						Name:  "PHP_FPM_SCRAPE_URI",
+						Value: "unix:///var/run/drupal.sock;/_site/_php-fpm-status",
 					},
 				}
-				currentobject.Spec.Template.Spec.Containers[i].Resources = drupallogsResources
+				currentobject.Spec.Template.Spec.Containers[i].VolumeMounts = []corev1.VolumeMount{
+					{
+						Name:      "empty-dir",
+						MountPath: "/var/run/",
+					},
+				}
+				currentobject.Spec.Template.Spec.Containers[i].Resources = phpfpmexporterResources
 			}
 		}
 
@@ -799,8 +802,8 @@ func deploymentForDrupalSite(currentobject *appsv1.Deployment, databaseSecret st
 				currentobject.Spec.Template.Spec.Containers[i].Image = NginxImage + ":" + releaseID
 			case "php-fpm":
 				currentobject.Spec.Template.Spec.Containers[i].Image = sitebuilderImageRefToUse(d, releaseID).Name
-			case "drupal-logs":
-				currentobject.Spec.Template.Spec.Containers[i].Image = sitebuilderImageRefToUse(d, releaseID).Name
+			case "php-fpm-exporter":
+				currentobject.Spec.Template.Spec.Containers[i].Image = "gitlab-registry.cern.ch/drupal/paas/php-fpm-prometheus-exporter:RELEASE.2021.06.02T09-41-38Z"
 			}
 		}
 	}
@@ -861,7 +864,7 @@ func persistentVolumeClaimForDrupalSite(currentobject *corev1.PersistentVolumeCl
 	return nil
 }
 
-// serviceForDrupalSite returns a service object for Nginx
+// serviceForDrupalSite returns a service object
 func serviceForDrupalSite(currentobject *corev1.Service, d *webservicesv1a1.DrupalSite) error {
 	if currentobject.Labels == nil {
 		currentobject.Labels = map[string]string{}
@@ -886,6 +889,12 @@ func serviceForDrupalSite(currentobject *corev1.Service, d *webservicesv1a1.Drup
 				TargetPort: intstr.FromInt(8081),
 				Name:       "webdav",
 				Port:       81,
+				Protocol:   "TCP",
+			},
+			{
+				TargetPort: intstr.FromInt(9253),
+				Name:       "php-fpm-exporter",
+				Port:       9253,
 				Protocol:   "TCP",
 			}}
 	}
@@ -1193,7 +1202,7 @@ func updateConfigMapForPHPFPM(ctx context.Context, currentobject *corev1.ConfigM
 		updateDeploymentAnnotations := func(deploy *appsv1.Deployment, d *webservicesv1a1.DrupalSite) error {
 			hash := md5.Sum([]byte(currentobject.Data["zz-docker.conf"]))
 			currentHash, flag := deploy.Spec.Template.ObjectMeta.Annotations["phpfpm-configmap/hash"]
-      // NOTE: the following check is unnecessary, we can always perform the action
+			// NOTE: the following check is unnecessary, we can always perform the action
 			if flag == false || hex.EncodeToString(hash[:]) != currentHash {
 				deploy.Spec.Template.ObjectMeta.Annotations["phpfpm-configmap/hash"] = hex.EncodeToString(hash[:])
 			}
