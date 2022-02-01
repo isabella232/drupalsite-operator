92122a6e9ba8bd5081f2f21bc00ed02f9d624402
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2022-02-01 17:22:33 +0100

    format

--- controllers/drupalsite_resources.go
+++ controllers/drupalsite_resources.go
@@ -247,11 +247,11 @@ func (r *DrupalSiteReconciler) ensureResources(drp *webservicesv1a1.DrupalSite, 
 		transientErrs = append(transientErrs, transientErr.Wrap("%v: for Nginx SVC"))
 	}
 	/* A new drupalsite can be initialized with 3 different ways depending its Spec:
-	- clone_job if Spec.Configuration.CloneFrom is given
-	- easystart_taskrun if Spec.Configuration.Easystart equals to enable
-	- site_install_job if it is a fresh site
-        Between CloneFrom and Easystart we don't care which case is checked first (undefined).
-        We use an OPA rule that prohibits both fields from being set at the same time.
+		- clone_job if Spec.Configuration.CloneFrom is given
+		- easystart_taskrun if Spec.Configuration.Easystart equals to enable
+		- site_install_job if it is a fresh site
+	        Between CloneFrom and Easystart we don't care which case is checked first (undefined).
+	        We use an OPA rule that prohibits both fields from being set at the same time.
 	*/
 	if r.isDBODProvisioned(ctx, drp) && !(drp.ConditionTrue("Initialized")) {
 		switch {
